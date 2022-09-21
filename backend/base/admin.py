9a434457ae4ded8e8f6eed52f94293827afc6b1e
ramzy05 <ramsesaguegang@gmail.com>
2022-09-21 01:14:05 +0100

    creation of models, serializer

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import (Product, Review, Order, OrderItem, ShippingAdress)
 # Register your models here.
+
+admin.site.register([Product, Review, Order, OrderItem, ShippingAdress])
